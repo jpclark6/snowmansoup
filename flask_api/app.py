dc663c7bdf1451b5ff97e759aa7f1d5594298471
Justin Clark <jpclark6@gmail.com>
2020-12-02 14:50:01 -0700

    created CR functionality

--- flask_api/app.py
+++ flask_api/app.py
@@ -6,25 +6,40 @@ import boto3
 from flask import request, jsonify
 from flask_lambda import FlaskLambda
 
+REGION = os.environ['REGION_NAME']
+TABLE_NAME = os.environ['TABLE_NAME']
+
 app = FlaskLambda(__name__)
+
+dynamodb = boto3.resource('dynamodb', region_name=REGION)
+
+def db_table(table_name=TABLE_NAME):
+    return dynamodb.Table(table_name)
 
 def parse_user_id(req):
     return req.headers['Authorization'].split()[1]
 
-@app.route('/sunshine')
-def melt_one_soup():
+@app.route('/soups')
+def fetch_soups():
     try:
         user_id = parse_user_id(request)
     except:
         return jsonify('Unauthorized'), 401
 
-    return jsonify('Melted a snowman')
+    table_response = db_table().query(KeyConditionExpression=Key('userId').eq(user_id))
+    return jsonify(table_response['Items'])
 
-@app.route('/summer')
-def melt_all_soup():
+@app.route('/soups', method=('POST',))
+def create_soup():
     try:
         user_id = parse_user_id(request)
     except:
-        return jsonify('Unquthorized'), 401
+        return jsonify('Unauthorized'), 401
 
-    return jsonify('All the snowmen melted')
+    soup_id = str(uuid.uuid4())
+    soup_data = request.get_json()
+    soup_data.update(userId=user_id, soupId=soup_id)
+    table = db_table()
+    table.put_item(Item=soup_data)
+    table_response = table.get_item(Key={'userId': user_id, 'soupId': soup_id})
+    return jsonify(table_response['Item']), 201
